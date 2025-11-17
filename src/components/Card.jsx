a532ae47489b2d607f4d05806b48bbceb13e10e0
Piama <athelec@gmail.com>
2025-11-17 14:21:19 +0100

    card colors, need to be updated again

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,25 +4,15 @@ const Card = ({ title, subtitle, tech, link }) => {
   return (
     <div className="w-[200px] h-[300px] relative border border-solid border-white/40 rounded-2xl overflow-hidden group">
       {/* --- bg --- */}
-      <div className="w-full h-full p-1 absolute bg-purple-400">
-        <div className="w-full h-full rounded-xl rounded-tr-[100px] rounded-br-[40px] bg-[#ff91d5]" />
+      <div className="w-full h-full p-1 absolute bg-[linear-gradient(to_bottom_right,var(--card-bg-start),var(--card-bg-end))]">
       </div>
-
-      {/* --- cercle orange --- */}
-      <div className="w-full h-full flex items-center justify-center relative backdrop-blur-lg rounded-2xl">
-        <div
-          className="w-32 h-32 rounded-full bg-gradient-to-tr from-pink-500 to-blue-300 animate-spin"
-          style={{ animationDuration: "12s" }}
-        />
-      </div>
-
       {/* --- contenu carte --- */}
       <div className="w-full h-full p-2 flex justify-between absolute inset-0">
-        <div className="w-3/5 p-2 pt-3 pb-1.5 flex flex-col rounded-xl backdrop-blur-lg bg-gray-50/10 text-gray-200 font-medium font-mono z-10">
-          <span className="text-xl text-black font-medium">{title}</span>
-          <span className="text-xs text-black">{subtitle}</span>
-          <span className="text-xs text-black mt-auto">{tech}</span>
-        </div>
+      <div className="w-3/5 p-2 pt-3 pb-1.5 flex flex-col rounded-xl backdrop-blur-lg bg-white/5 text-[#E2E2E2] font-medium font-mono z-10">
+        <span className="text-xl text-[var(--card-tech)] font-semibold">{title}</span>
+        <span className="text-xs text-[var(--card-text)]">{subtitle}</span>
+        <span className="text-xs text-[var(--card-tag)] mt-auto">{tech}</span>
+      </div>
 
         <div className="h-full pt-2 flex flex-col items-end text-white/50">
           {/* --- flèche cliquable --- */}
